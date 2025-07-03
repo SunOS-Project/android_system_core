68d57125d67cea3bfd2a4bb618053b4f26141e8a
chandu078 <chandudyavanapelli03@gmail.com>
2025-07-03 10:20:06 +0530

    Revert "init: Weaken property override security only when spoofing safetynet"
    This reverts commit 710531975bdc7e32422c0dbd31ac46f67742fa03.

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -922,9 +922,6 @@ static const char *snet_prop_value[] = {
 };
 
 static void workaround_snet_properties() {
-    // Weaken property override security to set safetynet props
-    weaken_prop_override_security = true;
-
 	std::string error;
 	LOG(INFO) << "snet: Hiding sensitive props";
 
@@ -932,9 +929,6 @@ static void workaround_snet_properties() {
 	for (int i = 0; snet_prop_key[i]; ++i) {
 		PropertySetNoSocket(snet_prop_key[i], snet_prop_value[i], &error);
 	}
-
-    // Restore the normal property override security after safetynet props have been set
-    weaken_prop_override_security = false;
 }
 
 // If the ro.product.[brand|device|manufacturer|model|name] properties have not been explicitly
@@ -1409,6 +1403,9 @@ void PropertyLoadBootDefaults() {
         }
     }
 
+    // Weaken property override security during execution of the vendor init extension
+    weaken_prop_override_security = true;
+
     // Update with vendor-specific property runtime overrides
     vendor_load_properties();
 
@@ -1423,6 +1420,9 @@ void PropertyLoadBootDefaults() {
 
     // Workaround SafetyNet
     workaround_snet_properties();
+
+    // Restore the normal property override security after init extension is executed
+    weaken_prop_override_security = false;
 }
 
 bool LoadPropertyInfoFromFile(const std::string& filename,
